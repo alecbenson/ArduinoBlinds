d1a360c6474e66176a849afc07ca9dad1ecb7d2a
Alec Benson <abenson@wpi.edu>
2014-07-21 23:51:10 -0400

    Fixed indenting

--- Serial_read.ino
+++ Serial_read.ino
@@ -4,27 +4,27 @@ int index = 0;
 int lastIndex = 0;
 
 void setup() {
-        Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 
 void loop() {
-
-        //Read in data from serial
-        if (Serial.available() > 0) {
-                incomingByte = Serial.read();
-                incomingData[index] = incomingByte;
-                index++;
-        }
-        
-        //If nothing has changed since last iteration, print the string
-        if(index == lastIndex && index != 0){
-          Serial.println(incomingData);
-          memset(incomingData, 0, sizeof(incomingData) );
-          index = 0;
-          lastIndex = 0;
-        }
-        
-        lastIndex = index;
-        delay(1000);
+  
+  //Read in data from serial
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    incomingData[index] = incomingByte;
+    index++;
+  }
+  
+  //If nothing has changed since last iteration, print the string
+  if(index == lastIndex && index != 0){
+    Serial.println(incomingData);
+    memset(incomingData, 0, sizeof(incomingData) );
+    index = 0;
+    lastIndex = 0;
+  }
+  
+  lastIndex = index;
+  delay(1000);
 }
